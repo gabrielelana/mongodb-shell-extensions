04052a0f99d8e8a36e79912951a8a13b9352cd52
gabriele.lana <gabriele.lana@cleancode.it>
2013-12-31 18:01:01 +0100

    initial packages

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function(grunt) {
+
+  var chalk = require('grunt-attention/node_modules/chalk')
+
+  grunt.initConfig({
+    bower_concat: {
+      dist: {
+        dest: '.work/bower_components.js'
+      }
+    },
+    concat: {
+      dist: {
+        dest: '.dist/mongorc.js',
+        src: ['.work/*.js', 'src/**/*.js']
+      }
+    },
+    copy: {
+      dist: {
+        src: '<%= concat.dist.dest %>',
+        dest: process.env.HOME + '/.mongorc.js',
+      }
+    },
+    attention: {
+      installed: {
+        options: {
+          borderColor: 'bgGreen',
+          message: 
+            chalk.green.bold('MongoDB shell extensions installed in your home directory (see <%= copy.dist.dest %>)') + "\n" +
+            chalk.green('next time you\'ll open your mongo shell you\'ll have all the extensions automatically loaded')
+        }
+      }
+    },
+    clean: ['.work', '.dist']
+  });
+
+  grunt.loadNpmTasks('grunt-bower-concat')
+  grunt.loadNpmTasks('grunt-contrib-concat')
+  grunt.loadNpmTasks('grunt-contrib-clean')
+  grunt.loadNpmTasks('grunt-contrib-copy')
+  grunt.loadNpmTasks('grunt-attention')
+
+  // TODO: jshint
+  grunt.registerTask('build', ['clean', 'bower_concat', 'concat'])
+  grunt.registerTask('install', ['build','copy', 'attention:installed'])
+  grunt.registerTask('default', ['build'])
+}
---
+++ src/hello.js
@@ -0,0 +1,3 @@
+var hello = function() {
+  print('Hello, if you see this then all your MongoDB shell extensions are been loaded correctly')
+}
