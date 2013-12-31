2073f2b4bd05cafceb8ec73acf2ff2932249a213
gabriele.lana <gabriele.lana@cleancode.it>
2013-12-31 18:16:11 +0100

    add distinctAndCount to DBCollection

---
+++ src/distinct.js
@@ -0,0 +1,18 @@
+
+DBCollection.prototype.distinctAndCount = function(field, query) {
+  query = query || {}
+
+  var it = this.aggregate(
+    {$match: query},
+    {$group: {_id: '$' + field, count: {$sum: 1}}},
+    {$project: {name: '$_id', count: 1, _id: 0}}
+  )
+  
+  if (it.ok === 1) {
+    return _.reduce(it.result, function(all, r) {
+      all[r.name] = r.count
+      return all
+    }, {})
+  }
+  return it
+}
