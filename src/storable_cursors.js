90f8d9d1e218a95e746085158588f633c1a5bef4
gabriele.lana <gabriele.lana@cleancode.it>
2014-02-24 15:43:26 +0100

    Make sure you have a collection before to save

--- src/storable_cursors.js
+++ src/storable_cursors.js
@@ -1,8 +1,7 @@
-var se = se || {}
-
 DBQuery.prototype.save = function(collection) {
   collection = (typeof collection === 'string') ? this._db.getCollection(collection) : collection
-  collection = collection || se.collection()
+  collection = collection || this._db.createTemporaryCollection()
+  assert(collection && collection.constructor.name === 'DBCollection', 'need a collection')
   while (this.hasNext()) {
     collection.save(this.next())
   }
