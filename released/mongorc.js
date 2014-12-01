68fcc6ef3c03b1a7553e364c62f10827872bc370
gabriele.lana <gabriele.lana@cleancode.it>
2014-12-01 21:50:25 +0100

    Release 0.2.4

--- released/mongorc.js
+++ released/mongorc.js
@@ -10244,7 +10244,7 @@ moment.fn.within = function(range) {
 
 /* global chatty */
 
-chatty('\033[1;32m+ MongoDB Shell Extensions (0.2.3) by Gabriele Lana <gabriele.lana@gmail.com>\033[0m')
+chatty('\033[1;32m+ MongoDB Shell Extensions (0.2.4) by Gabriele Lana <gabriele.lana@gmail.com>\033[0m')
 
 DBCollection.prototype.last = function(n) {
   return this.find().sort({_id: -1}).limit(n || 1)
@@ -10303,38 +10303,53 @@ var printcsv = function(x) {
   })
 }
 
-var tocsv = function(x) {
-  var lines = [],
-      fieldNames = {},
-      encodedDocuments = x.map(function(doc) {
-        return _.reduce(doc, function(values, value, field) {
-          fieldNames[field] = true
-          values[field] = tojson(value).replace(
-            /^(?:ISODate|ObjectId)\((.*)\)$/,
-            function(_, contentAsString) {
-              return contentAsString
-            }
-          )
-          return values
-        }, {})
-      })
+var tocsv = (function() {
+  var flatten = function(o) {
+    return _.reduce(o, function(flattened, value, field) {
+      if (_.isPlainObject(value)) {
+        _.forEach(flatten(value), function(nestedValue, nestedField) {
+          flattened[[field, nestedField].join('.')] = nestedValue
+        })
+      } else {
+        flattened[field] = value
+      }
+      return flattened
+    }, {})
+  }
 
-  fieldNames = _.keys(fieldNames)
+  return function(x) {
+    var lines = [],
+        fieldNames = {},
+        encodedDocuments = x.map(function(doc) {
+          return _.reduce(flatten(doc), function(values, value, field) {
+            fieldNames[field] = true
+            values[field] = tojson(value).replace(
+              /^(?:ISODate|ObjectId)\((.*)\)$/,
+              function(_, contentAsString) {
+                return contentAsString
+              }
+            )
+            return values
+          }, {})
+        })
 
-  lines.push(fieldNames.join(','))
-  encodedDocuments.forEach(function(encodedDocument) {
-    lines.push(
-      fieldNames.map(function(fieldName) {
-        if (encodedDocument[fieldName] !== undefined) {
-          return encodedDocument[fieldName]
-        }
-        return '""'
-      }).join(',')
-    )
-  })
+    fieldNames = _.keys(fieldNames)
 
-  return new CSV(lines)
-}
+    lines.push(fieldNames.join(','))
+    encodedDocuments.forEach(function(encodedDocument) {
+      lines.push(
+        fieldNames.map(function(fieldName) {
+          if (encodedDocument[fieldName] !== undefined) {
+            return encodedDocument[fieldName]
+          }
+          return ''
+        }).join(',')
+      )
+    })
+
+    return new CSV(lines)
+  }
+})()
 
 DBQuery.prototype.reverse = function() {
   if (!this._query.query || _.isEmpty(this._query.query)) {
